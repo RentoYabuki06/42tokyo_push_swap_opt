e6b6886581dd8715b45f415d8588d2a2b0fc9220
Rento Yabuki <ryabuki@c2r1s2.42tokyo.jp>
2025-03-10 20:48:35 +0900

    norm

--- srcs/main.c
+++ srcs/main.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 21:09:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/03/10 20:43:45 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/03/10 20:48:01 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+static void	ft_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	if (ft_stack_len(*stack_a) == 2)
+		ft_sa(stack_a);
+	else if (ft_stack_len(*stack_a) == 3)
+		ft_sort_three(stack_a);
+	else
+		ft_sort_large(stack_a, stack_b);
+}
 
 int	main(int argc, char **argv)
 {
@@ -30,14 +40,7 @@ int	main(int argc, char **argv)
 	if (ft_init_stack_a(&stack_a, nbr))
 		ft_free_error(&stack_a, nbr);
 	if (!ft_is_sorted(stack_a))
-	{
-		if (ft_stack_len(stack_a) == 2)
-			ft_sa(&stack_a);
-		else if (ft_stack_len(stack_a) == 3)
-			ft_sort_three(&stack_a);
-		else
-			ft_sort_large(&stack_a, &stack_b);
-	}
+		ft_sort(&stack_a, &stack_b);
 	ft_free_stack(&stack_a);
 	ft_free_split(nbr);
 	return (EXIT_SUCCESS);
