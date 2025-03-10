d504b47a9319ff73f29672b341983af55bf76782
rento yabuki <rento.nky.1203@gmail.com>
2025-03-10 20:44:55 +0900

    splitをargvで受けることをやめました

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 21:09:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/03/10 20:06:02 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/03/10 20:43:45 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	char	**nbr;
 
 	stack_a = NULL;
 	stack_b = NULL;
+	nbr = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (EXIT_SUCCESS);
 	else if (argc == 2)
-		argv = ft_push_swap_split(argv[1], ' ');
-	if (ft_init_stack_a(&stack_a, argv + 1))
-		ft_free_error(&stack_a, argv);
+		nbr = ft_push_swap_split(argv[1], ' ');
+	else
+		nbr = argv + 1;
+	if (ft_init_stack_a(&stack_a, nbr))
+		ft_free_error(&stack_a, nbr);
 	if (!ft_is_sorted(stack_a))
 	{
 		if (ft_stack_len(stack_a) == 2)
@@ -35,6 +39,6 @@ int	main(int argc, char **argv)
 			ft_sort_large(&stack_a, &stack_b);
 	}
 	ft_free_stack(&stack_a);
-	ft_free_split(argv);
+	ft_free_split(nbr);
 	return (EXIT_SUCCESS);
 }
