379b42fd6dc871ef488a8e7465da6840d69b82be
rento yabuki <rento.nky.1203@gmail.com>
2025-03-10 17:38:59 +0900

    final

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/07 21:10:53 by yabukirento       #+#    #+#              #
-#    Updated: 2025/03/09 16:13:55 by yabukirento      ###   ########.fr        #
+#    Updated: 2025/03/10 17:36:26 by yabukirento      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,6 +26,7 @@ SRCS =	./srcs/required_functions/push.c \
 		./srcs/fill_stack_a.c \
 		./srcs/set_median.c \
 		./srcs/prep_for_push.c \
+		./srcs/cost_cal.c \
 
 OBJS = $(SRCS:.c=.o)
 
--- srcs/init_node_a.c
+++ srcs/init_node_a.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:18:47 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/03/10 15:12:40 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/03/10 17:34:01 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,48 +36,6 @@ static void	ft_set_target_a(t_stack *stack_a, t_stack *stack_b)
 			stack_a->target_node = ft_find_min(stack_b);
 		else
 			stack_a->target_node = target_node;
-		stack_a = stack_a->next;
-	}
-}
-
-static int	ft_calculate_position_cost(int index, bool above_median, int stack_len)
-{
-	if (above_median)
-		return (index);
-	return (stack_len - index);
-}
-
-static int	ft_get_total_cost(int cost_a, int cost_b, bool same_rotation)
-{
-	if (same_rotation)
-	{
-		if (cost_a > cost_b)
-			return (cost_a);
-		return (cost_b);
-	}
-	return (cost_a + cost_b);
-}
-
-static void	ft_cost_cal(t_stack *stack_a, t_stack *stack_b)
-{
-	int	len_a;
-	int	len_b;
-	int	cost_a;
-	int	cost_b;
-	bool	same_rotation;
-
-	len_a = ft_stack_len(stack_a);
-	len_b = ft_stack_len(stack_b);
-	while (stack_a)
-	{
-		cost_a = ft_calculate_position_cost(stack_a->index, \
-			stack_a->above_median, len_a);
-		cost_b = ft_calculate_position_cost(stack_a->target_node->index, \
-			stack_a->target_node->above_median, len_b);
-		same_rotation = (stack_a->above_median && \
-			stack_a->target_node->above_median) || \
-			(!stack_a->above_median && !stack_a->target_node->above_median);
-		stack_a->push_cost = ft_get_total_cost(cost_a, cost_b, same_rotation);
 		stack_a = stack_a->next;
 	}
 }
