0e4841acf2180e4232c99d8485515b28de7634fb
rento yabuki <rento.nky.1203@gmail.com>
2025-03-10 18:32:30 +0900

    deal with LINE TOO LONG

--- srcs/init_node_a.c
+++ srcs/init_node_a.c
@@ -6,13 +6,11 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:18:47 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/03/10 18:25:36 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/03/10 18:31:33 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-
 
 static int	ft_position_cost(int index, bool above_median, int stack_len)
 {
@@ -32,6 +30,15 @@ static int	ft_get_total_cost(int cost_a, int cost_b, bool same_rotation)
 	return (cost_a + cost_b);
 }
 
+static bool	ft_same_rotation(t_stack *stack_a)
+{
+	if (stack_a->above_median && stack_a->target_node->above_median)
+		return (true);
+	if (!stack_a->above_median && !stack_a->target_node->above_median)
+		return (true);
+	return (false);
+}
+
 static void	ft_cost_cal(t_stack *stack_a, t_stack *stack_b)
 {
 	int		len_a;
@@ -48,9 +55,7 @@ static void	ft_cost_cal(t_stack *stack_a, t_stack *stack_b)
 			stack_a->above_median, len_a);
 		cost_b = ft_position_cost(stack_a->target_node->index, \
 			stack_a->target_node->above_median, len_b);
-		same_rotation = (stack_a->above_median && \
-			stack_a->target_node->above_median) || \
-			(!stack_a->above_median && !stack_a->target_node->above_median);
+		same_rotation = ft_same_rotation(stack_a);
 		stack_a->push_cost = ft_get_total_cost(cost_a, cost_b, same_rotation);
 		stack_a = stack_a->next;
 	}
