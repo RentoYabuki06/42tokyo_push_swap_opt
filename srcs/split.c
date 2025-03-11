0ce1cc25472a85c491951acb5a98badecddf6435
rento yabuki <rento.nky.1203@gmail.com>
2025-03-11 16:59:28 +0900

    separate split func to deal with norm

--- srcs/split.c
+++ srcs/split.c
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:45:18 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/03/11 16:47:54 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/03/11 16:58:24 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,34 +53,38 @@ static char	*ft_get_next_word(char *s, char c, int *cursor, int max_len)
 		++len;
 	next_word = malloc(sizeof(char) * (len + 1));
 	if (!next_word)
-		return (NULL);	
+		return (NULL);
 	while (i < len && s[*cursor])
 		next_word[i++] = s[(*cursor)++];
 	next_word[i] = '\0';
 	return (next_word);
 }
 
-char	**ft_push_swap_split(char *s, char c)
+static char	**allocate_and_handle_error(char *s, int words_count)
 {
-	int		words_count;
 	char	**result_array;
-	int		i;
-	int		cursor;
-	int		str_len;
 
 	if (!s)
 		return (NULL);
-	str_len = 0;
-	while (s[str_len])
-		str_len++;
-	i = 0;
-	cursor = 0;
-	words_count = ft_count_words(s, c);
 	if (!words_count)
 		exit(1);
 	result_array = malloc(sizeof(char *) * (words_count + 1));
 	if (!result_array)
 		return (NULL);
+	return (result_array);
+}
+
+static char	**split_into_words(char *s, char c, int words_count, int str_len)
+{
+	char	**result_array;
+	int		i;
+	int		cursor;
+
+	result_array = allocate_and_handle_error(s, words_count);
+	if (!result_array)
+		return (NULL);
+	i = 0;
+	cursor = 0;
 	while (i < words_count)
 	{
 		result_array[i] = ft_get_next_word(s, c, &cursor, str_len);
@@ -95,4 +99,16 @@ char	**ft_push_swap_split(char *s, char c)
 	}
 	result_array[i] = NULL;
 	return (result_array);
+}
+
+char	**ft_push_swap_split(char *s, char c)
+{
+	int		words_count;
+	int		str_len;
+
+	str_len = 0;
+	while (s && s[str_len])
+		str_len++;
+	words_count = ft_count_words(s, c);
+	return (split_into_words(s, c, words_count, str_len));
 }
